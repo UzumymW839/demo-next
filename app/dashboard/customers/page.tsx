5f1e807f81515712cac3fb9650653a6d7cd53b20
rmetzger <robert.metzger@offis.de>
2025-11-03 15:56:10 +0100

    new branch on new account

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+export default function Page () {
+    return <div>Placeholder
+        <p>This is the customers page.</p>
+    </div>;
+
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+export default function Page () {
+    return <div>Placeholder
+        <p>This is the invoices page.</p>
+    </div>;
+
+}
